eb10b0f065b9f6c4d05db9968d83bb4bc4afced8
Hor Tengmeng <tengmeng382@gmail.com>
2026-03-05 08:03:22 +0700

    add ci/cd workflow

---
+++ src/main/java/com/meng/testcicd/user/UserController.java
@@ -0,0 +1,22 @@
+package com.meng.testcicd.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired private UserRepository repo;
+
+    @GetMapping
+    public List<User> getAll() { return repo.findAll(); }
+
+    @PostMapping
+    public User create(@RequestBody User user) { return repo.save(user); }
+}
---
+++ src/main/java/com/meng/testcicd/user/UserService.java
@@ -0,0 +1,5 @@
+package com.meng.testcicd.user;
+
+public class UserService {
+    
+}
